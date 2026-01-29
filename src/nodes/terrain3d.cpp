d30a514861b9a9248d2744788e8d11e295435a4b
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2026-01-29 14:03:19 +0100

    cleanup terrain3d and wip terrain_renderer

--- src/nodes/terrain3d.cpp
+++ src/nodes/terrain3d.cpp
@@ -1,7 +1,6 @@
 #include "terrain3d.h"
 
 #include "godot_cpp/classes/engine.hpp"
-#include "godot_cpp/classes/surface_tool.hpp"
 
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
@@ -11,74 +10,43 @@ using namespace godot;
 namespace ts {
 
 void Terrain3D::_bind_methods() {
-	// --- Configuration (Resource) ---
 	ClassDB::bind_method(D_METHOD("set_configuration", "config"), &Terrain3D::set_configuration);
 	ClassDB::bind_method(D_METHOD("get_configuration"), &Terrain3D::get_configuration);
+	ClassDB::bind_method(D_METHOD("get_generator"), &Terrain3D::get_generator);
+	ClassDB::bind_method(D_METHOD("get_renderer"), &Terrain3D::get_renderer);
 	ClassDB::bind_method(D_METHOD("_update_generator"), &Terrain3D::_update_generator);
 
-	ADD_GROUP("Data Source", "");
 	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "configuration", PROPERTY_HINT_RESOURCE_TYPE, "TerrainConfiguration"), "set_configuration", "get_configuration");
-
-	// --- Visuals (Node Parameters) ---
-	ClassDB::bind_method(D_METHOD("set_mesh_resolution", "resolution"), &Terrain3D::set_mesh_resolution);
-	ClassDB::bind_method(D_METHOD("get_mesh_resolution"), &Terrain3D::get_mesh_resolution);
-
-	ClassDB::bind_method(D_METHOD("set_material", "material"), &Terrain3D::set_material);
-	ClassDB::bind_method(D_METHOD("get_material"), &Terrain3D::get_material);
-
-	ADD_GROUP("Visuals", "");
-	// Range from 2 to 256 vertex
-	ADD_PROPERTY(PropertyInfo(Variant::INT, "mesh_resolution", PROPERTY_HINT_RANGE, "2,256,1"), "set_mesh_resolution", "get_mesh_resolution");
-	// Slot for material override
-	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "material", PROPERTY_HINT_RESOURCE_TYPE, "Material"), "set_material", "get_material");
 }
 
 void Terrain3D::_update_generator() {
 	if (_config.is_valid() && _generator.is_valid()) {
 		_generator->setup(_config);
-		_generate_mesh();
-		UtilityFunctions::print("TerrainServer:: Generator configuration updated.");
-	}
-}
-
-void Terrain3D::_generate_mesh() {
-	if (!_generator.is_valid()) {
-		return;
-	}
-
-	if (_mesh_instance == nullptr) {
-		_mesh_instance = memnew(MeshInstance3D);
-		add_child(_mesh_instance);
-		_mesh_instance->set_name("TerrainMesh");
-		_mesh_instance->set_owner(get_owner());
-	}
-
-	float size = _mesh_resolution * 1.0f;
-	Ref<ArrayMesh> mesh = _generator->generate_mesh(_mesh_resolution, size);
-	_mesh_instance->set_mesh(mesh);
-
-	// Apply material if set
-	if (_material_override.is_valid()) {
-		_mesh_instance->set_material_override(_material_override);
+		_renderer->set_generator(_generator);
+		_renderer->update_mesh();
+		UtilityFunctions::print("Terrain3D: Generator configuration updated.");
 	}
 }
 
 Terrain3D::Terrain3D() {
 	_generator.instantiate();
-	UtilityFunctions::print("TerrainServer: initialized");
+	_renderer.instantiate();
+	UtilityFunctions::print("Terrain3D: Initialized");
 }
 
 Terrain3D::~Terrain3D() {
-	_mesh_instance = nullptr;
-	UtilityFunctions::print("TerrainServer: destroyed");
+	UtilityFunctions::print("Terrain3D: Destroyed");
+}
+
+void Terrain3D::_ready() {
+	_renderer->initialize(this);
+	_update_generator();
 }
 
 void Terrain3D::_process(double delta) {
 	if (Engine::get_singleton()->is_editor_hint()) {
 		return;
 	}
-
-	UtilityFunctions::print("Height at (0,0): ", _generator->get_height(0.0f, 0.0f));
 }
 
 Ref<TerrainConfiguration> Terrain3D::get_configuration() const {
@@ -86,20 +54,16 @@ Ref<TerrainConfiguration> Terrain3D::get_configuration() const {
 }
 
 void Terrain3D::set_configuration(const Ref<TerrainConfiguration> &p_config) {
-	// If the same config, do nothing
 	if (_config == p_config) {
 		return;
 	}
 
-	// Disconnect from previous config signals
 	if (_config.is_valid()) {
 		_config->disconnect("changed", Callable(this, "_update_generator"));
 	}
 
-	// Set new config
 	_config = p_config;
 
-	// Connect to new config signals
 	if (_config.is_valid()) {
 		_config->connect("changed", Callable(this, "_update_generator"));
 	}
@@ -107,37 +71,12 @@ void Terrain3D::set_configuration(const Ref<TerrainConfiguration> &p_config) {
 	_update_generator();
 }
 
-int Terrain3D::get_mesh_resolution() const {
-	return _mesh_resolution;
+Ref<TerrainGenerator> Terrain3D::get_generator() const {
+	return _generator;
 }
 
-void Terrain3D::set_mesh_resolution(int p_resolution) {
-	if (p_resolution < 2) {
-		p_resolution = 2;
-	}
-
-	if (p_resolution == _mesh_resolution) {
-		return;
-	}
-
-	_mesh_resolution = p_resolution;
-	_generate_mesh();
-}
-
-Ref<Material> Terrain3D::get_material() const {
-	return _material_override;
-}
-
-void Terrain3D::set_material(const Ref<Material> &p_material) {
-	if (_material_override == p_material) {
-		return;
-	}
-
-	_material_override = p_material;
-
-	if (_mesh_instance != nullptr) {
-		_mesh_instance->set_material_override(_material_override);
-	}
+Ref<TerrainRenderer> Terrain3D::get_renderer() const {
+	return _renderer;
 }
 
 } //namespace ts
