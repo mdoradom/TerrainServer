192c3a1c43415dbd0ef0ba499ec4756c98c8a461
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2026-01-30 15:31:24 +0100

    check if renderer is valid before update_generator

--- src/nodes/terrain3d.cpp
+++ src/nodes/terrain3d.cpp
@@ -20,7 +20,7 @@ void Terrain3D::_bind_methods() {
 }
 
 void Terrain3D::_update_generator() {
-	if (_config.is_valid() && _generator.is_valid()) {
+	if (_config.is_valid() && _generator.is_valid() && _renderer.is_valid()) {
 		_generator->setup(_config);
 		_renderer->set_generator(_generator);
 		_renderer->set_mesh_resolution(_config->get_mesh_resolution());
